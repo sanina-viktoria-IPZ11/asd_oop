a1a606bc85e8cec6b0c65670c9738d2fdeab423f
sanina-viktoria-IPZ11 <99338697+sanina-viktoria-IPZ11@users.noreply.github.com>
2022-04-22 17:07:29 +0300

    Add files via upload

---
+++ asd1/LinkedList.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+
+    public class LinkedList
+    {
+        public LinkedList Next;
+        public int Data;
+        public LinkedList(int data, LinkedList next)
+        {
+            Data = data;
+            Next = next;
+        }
+    }
+}
